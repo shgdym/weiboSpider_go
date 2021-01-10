2d295ed25dd6033a0c10c3820cb74c30cc0196f9
shgdym <shgdym@hotmail.com>
2021-01-10 21:36:41 +0800

    去除html标签

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	f "./func"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -11,37 +12,42 @@ func main() {
 	var weiboUid string
 	var isok float64
 
-	isok = 1
-	has_nextpage := true
-
 	fmt.Print("please type uid: ")
 	fmt.Scanln(&weiboUid)
 	sinceId := ""
 
-	for has_nextpage {
-		pageContent := getPageContent(weiboUid, sinceId)
+	isok = 1
+	get_nextpage := true
 
+	// 去除html标签
+	re, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+
+	for get_nextpage {
+		// 获取api返回信息 默认sinceId为空 即返回第一页
+		pageContent := getPageContent(weiboUid, sinceId)
 		pageMap := f.JsonToMap(pageContent)
 
-		data := pageMap["data"].(map[string]interface{})["cards"]
-
+		// 异常
 		if isok != pageMap["ok"] {
 			break
 		}
+
+		// 解析数据
+		data := pageMap["data"].(map[string]interface{})["cards"]
 		for _, v := range data.([]interface{}) {
 			item := v.(map[string]interface{})
 			mblog := item["mblog"].(map[string]interface{})
-			id := mblog["id"]
-			sinceId = id.(string)
-			text := mblog["text"]
+			text := re.ReplaceAllString(mblog["text"].(string), "") // 微博内容
 			fmt.Println(text)
+			sinceId = mblog["id"].(string)
 		}
 
+		// 是否获取下一页
 		fmt.Print("next page(y/n):")
 		var type_res string
 		fmt.Scanln(&type_res)
 		if strings.ToLower(type_res) != "y" {
-			has_nextpage = false
+			get_nextpage = false
 		}
 	}
 
@@ -50,6 +56,7 @@ func main() {
 	os.Stdin.Read(b)
 }
 
+// 拼接url, 进行网络请求
 func getPageContent(weiboUid string, sinceId string) string {
 	url := "https://m.weibo.cn/api/container/getIndex?uid=" + weiboUid + "&t=0&type=uid&value=" + weiboUid + "&containerid=107603" + weiboUid + "&since_id=" + sinceId
 	return f.GetHttpResult(url)
